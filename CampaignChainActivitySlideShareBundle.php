86cad2c3126708a89cc6b97311529adcbc46e2ea
Sandro Groganz <sandro@campaignchain.com>
2015-06-19 16:02:57 +0200

    initial commit

---
+++ CampaignChainActivitySlideShareBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace CampaignChain\Activity\SlideShareBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CampaignChainActivitySlideShareBundle extends Bundle
+{
+}
---
+++ Controller/SlideShareController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace CampaignChain\Activity\SlideShareBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class SlideShareController extends Controller
+{
+
+    public function newAction(Request $request)
+    {
+
+    }
+
+    public function editAction(Request $request, $id)
+    {
+    
+    }
+
+    public function editModalAction(Request $request, $id)
+    {
+    
+    }
+
+    public function editApiAction(Request $request, $id)
+    {
+    
+    }
+
+    public function readAction(Request $request, $id)
+    {
+    
+    }
+}
