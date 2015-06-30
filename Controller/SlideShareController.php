373df10a6d5a14e2abbe50d617ad52df2252228f
vaop <vaop@users.noreply.github.com>
2015-06-30 18:15:21 +0530

    Various changes

--- Controller/SlideShareController.php
+++ Controller/SlideShareController.php
@@ -12,12 +12,46 @@ namespace CampaignChain\Activity\SlideShareBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use CampaignChain\Location\SlideShareBundle\Entity\SlideShareUser;
 
 class SlideShareController extends Controller
 {
 
+    const ACTIVITY_BUNDLE_NAME          = 'campaignchain/activity-slideshare';
+    const ACTIVITY_MODULE_IDENTIFIER    = 'campaignchain-slideshare';
+    const OPERATION_BUNDLE_NAME         = 'campaignchain/operation-slideshare';
+    const OPERATION_MODULE_IDENTIFIER   = 'campaignchain-slideshare';
+    const LOCATION_BUNDLE_NAME          = 'campaignchain/location-slideshare';
+    const LOCATION_MODULE_IDENTIFIER    = 'campaignchain-slideshare';
+    const TRIGGER_HOOK_IDENTIFIER       = 'campaignchain-due';
+
     public function newAction(Request $request)
     {
+        $wizard = $this->get('campaignchain.core.activity.wizard');
+        $campaign = $wizard->getCampaign();
+        $activity = $wizard->getActivity();
+        $location = $wizard->getLocation();
+
+        $activity->setEqualsOperation(true);
+        
+        // retrieve slide decks
+        $client = $this->get('campaignchain.channel.slideshare.rest.client');
+        $connection = $client->connectByLocation($location);
+        $xml = $connection->getUserSlideshows();
+
+        
+        if($upcomingNewsletters['total'] == 0){
+            $this->get('session')->getFlashBag()->add(
+                'warning',
+                'No upcoming newsletter campaigns available.'
+            );
+
+            return $this->redirect(
+                $this->generateUrl('campaignchain_core_activities_new')
+            );
+        }
+
+        $form->handleRequest($request);
 
     }
 
